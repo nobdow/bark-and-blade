c9b82eae07b6fdeae9b116cea7aae26c3b00c59c
noble <nobdow316@gmail.com>
2024-02-20 22:03:30 -0600

    0.92

--- src/Base_Elements/Player.java
+++ src/Base_Elements/Player.java
@@ -2,15 +2,6 @@ package Base_Elements;
 
 import Equipment.*;
 
-//public class Player {
-//
-//    public static int hp;
-//    public static int maxHP;
-//    public SuperWeapon currentWeapon;
-//}
-
-import java.util.ArrayList;
-
 public class Player {
 
     public static String playerName;
@@ -18,29 +9,5 @@ public class Player {
     public static String dogName;
     public static int hp;
     public static int maxHP;
-    public int weaponCapacity;
     public SuperWeapon currentWeapon;
-    public ArrayList<SuperWeapon> weapons;
-
-    public boolean isOverburdened() {
-        // -1 accounts for the 0 index
-        return (this.weapons.size() >= (this.weaponCapacity - 1));
-    }
-
-    public void setCurrentWeapon(SuperWeapon w){
-        this.currentWeapon = w;
-    }
-
-    // returns true if weapon was added successfully
-    public boolean addWeapon(SuperWeapon w) {
-        if (!this.isOverburdened()) {
-            weapons.add(w);
-            return true;
-        }
-        return false;
-    }
-
-    public SuperWeapon getCurrentWeapon() {
-        return this.currentWeapon;
-    }
 }
